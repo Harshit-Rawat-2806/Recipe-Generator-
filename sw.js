cadf8cb36ee7be34e1a8d9f1c67445ab59710cbc
Harshit Rawat <rawatharshit685@gmail.com>
2025-10-24 13:42:38 +0530

    Initial commit without API keys

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'namak-shamak-v1';
+const urlsToCache = [
+  '/',
+  '/App.html',
+  '/namak_logo.png',
+  'https://cdn.tailwindcss.com'
+];
+
+// Install service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+// Update service worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
